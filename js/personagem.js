90f9be38dc27433e0acc7f2c9a43b66261cb25de
juliafilisbino <julia.filisbino69@gmail.com>
2024-07-02 17:10:47 -0300

    image.
    ble!!!!.

---
+++ js/personagem.js
@@ -0,0 +1,13 @@
+class Produto{
+    constructor(preco, nome, loja){
+        this.preco = preco;
+        this.nome = nome;
+        this.loja = loja;
+    }
+}
+
+let produtos = []
+for (var i = 1 ; i<=15 ; i++){
+    produtos.push(new Produto(i, "nome"+i, "loja"+i));
+}
+console.log(produtos);
